1a993e08529ea5f0817dbdaf26cf91cb9cc96c11
Nguyễn Duy Ngọc <92100963+Dautay2402@users.noreply.github.com>
2023-07-15 18:29:27 +0700

    modify

--- Solution/TFIDF/TFIDF.py
+++ Solution/TFIDF/TFIDF.py
@@ -4,49 +4,47 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# def compute_tf(text):
-#     # Tính toán tần suất thuật ngữ (term frequency)
-#     tf_text = Counter(text.split())
-#     for i in tf_text:
-#         tf_text[i] = tf_text[i]/float(len(text.split()))
-#     return tf_text
+def compute_tf(text):
+    tf_text = Counter(text.split())
+    for i in tf_text:
+        tf_text[i] = tf_text[i]/float(len(text.split()))
+    return tf_text
 
-# def compute_idf(word, corpus):
-#     # Tính toán đảo tần suất thuật ngữ độc lập (inverse document frequency)
-#     return math.log10(len(corpus)/sum([1.0 for i in corpus if word in i.split()]))
+def compute_idf(word, corpus):
+    return math.log10(len(corpus)/sum([1.0 for i in corpus if word in i.split()]))
 
-# def compute_tf_idf(corpus):
-#     # Tính toán TF-IDF cho tất cả các từ trong tập hợp các văn bản
-#     documents_list = []
-#     for text in corpus:
-#         tf_idf_dictionary = {}
-#         computed_tf = compute_tf(text)
-#         for word in computed_tf:
-#             tf_idf_dictionary[word] = computed_tf[word] * compute_idf(word, corpus)
-#         documents_list.append(tf_idf_dictionary)
-#     return documents_list
+def compute_tf_idf(corpus):
+    documents_list = []
+    for text in corpus:
+        print(text)
+        tf_idf_dictionary = {}
+        computed_tf = compute_tf(text)
+        for word in computed_tf:
+            tf_idf_dictionary[word] = computed_tf[word] * compute_idf(word, corpus)
+        documents_list.append(tf_idf_dictionary)
+    return documents_list
 
-# def cosine_similarity(vec1, vec2):
-#     intersection = set(vec1.keys()) & set(vec2.keys())
-#     numerator = sum([vec1[x] * vec2[x] for x in intersection])
+def cosine_simi(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
 
-#     sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
-#     sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
-#     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+    sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
+    sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
 
-#     if not denominator:
-#         return 0.0
-#     return float(numerator) / denominator
+    if not denominator:
+        return 0.0
+    return float(numerator) / denominator
 
-# def tfidf_similarity(text1, text2):
-#     with open("TF_IDF/corpus.txt", "r", encoding='utf-8') as file:
-#         lines = file.readlines()
+def tfidf_simi(text1, text2):
+    with open("TF_IDF/corpus.txt", "r", encoding='utf-8') as file:
+        lines = file.readlines()
 
-#     corpus = [line.strip() for line in lines]
-#     corpus.append(text1)
-#     corpus.append(text2)
-#     tfidf_matrix = compute_tf_idf(corpus)
-#     return cosine_similarity(tfidf_matrix[-2], tfidf_matrix[-1])
+    corpus = [line.strip() for line in lines]
+    corpus.append(text1)
+    corpus.append(text2)
+    tfidf_matrix = compute_tf_idf(corpus)
+    return cosine_simi(tfidf_matrix[-2], tfidf_matrix[-1])
 
 def tfidf_similarity(text1, text2):
     with open(r"D:\UIT\Năm 2\Kỳ 4\Tính toán đa phương tiện\Doc Simi\TF_IDF\corpus.txt", "r", encoding='utf-8') as file:
@@ -55,11 +53,12 @@ def tfidf_similarity(text1, text2):
     corpus = [line.strip() for line in lines]
     corpus.append(text1)
     corpus.append(text2)
-    vectorizer = TfidfVectorizer(stop_words='english', smooth_idf=True)
+    vectorizer = TfidfVectorizer(smooth_idf=True)
     tfidf_matrix = vectorizer.fit_transform(corpus)
     cosine_sim = cosine_similarity(tfidf_matrix[-2], tfidf_matrix[-1])[0][0]
     return cosine_sim
 
 text1 = 'I have a dog and i love it'
-text2 = 'I have a dog and i love it'
+text2 = 'i have a cat and i hate it'
 print(tfidf_similarity(text1, text2))
+print(tfidf_simi(text1, text2))
