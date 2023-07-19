40f13a96eb37f3c11d38bd94cb3b9a162727f37f
Nguyễn Duy Ngọc <92100963+Dautay2402@users.noreply.github.com>
2023-07-20 01:27:37 +0700

    modify

--- Solution/Dice/Dice.py
+++ Solution/Dice/Dice.py
@@ -15,12 +15,6 @@ def dice_similarity(doc1, doc2):
     """Calculate Jaccard and Dice coefficients"""
     terms1, terms2 = dice_unique_terms(doc1, doc2)
     intersection = [value for value in terms1 if value in terms2]
-    union = terms1.copy()
-    for term in terms2:
-        if term not in union:
-            union.append(term)       
-    if len(union) == 0:
-        return 0
     dice_coefficient = float(2 * len(intersection)) / (len(terms1) + len(terms2))
     return dice_coefficient
 
