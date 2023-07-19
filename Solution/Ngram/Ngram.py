122d6a25bb5835872224fa15da42e55a6dce006b
Nguyễn Duy Ngọc <92100963+Dautay2402@users.noreply.github.com>
2023-07-20 01:38:06 +0700

    modify

--- Solution/Ngram/Ngram.py
+++ Solution/Ngram/Ngram.py
@@ -1,21 +1,4 @@
-
-# code chay
-def generate_ngrams(text, n):
-    words = text.lower().split()
-    ngrams = []
-    for i in range(len(words) - n + 1):
-        ngram = ' '.join(words[i:i+n])
-        ngrams.append(ngram)
-    return ngrams
-
-def ngram_similarity(text1, text2, n):
-    ngrams1 = set(generate_ngrams(text1, n))
-    ngrams2 = set(generate_ngrams(text2, n))
-    common_ngrams = ngrams1.intersection(ngrams2)
-    
-    similarity = len(common_ngrams) / float(max(len(ngrams1), len(ngrams2)))
-    return similarity
-
+import math
 # code sử dụng thư viện
 # from nltk import ngrams
 # from sklearn.feature_extraction.text import CountVectorizer
@@ -35,6 +18,40 @@ def ngram_similarity(text1, text2, n):
 #     similarity_matrix = cosine_similarity(vectorized_corpus)
 #     similarity = similarity_matrix[0, 1]
 #     return similarity
+
+def tokenize_text(text, n):
+    words = text.lower().split()
+    tokens = [' '.join(words[i:i+n]) for i in range(len(words)-(n-1))]
+    return tokens
+
+def create_vocabulary(tokens1, tokens2):
+    vocabulary = set(tokens1 + tokens2)
+    return vocabulary
+
+def document_representation(ngrams, vocabulary):  
+    document_vector = {}
+    for ngram in vocabulary:
+        if ngram in ngrams:
+            if ngram in document_vector:
+                document_vector[ngram] += 1
+            else:
+                document_vector[ngram] = 1
+    
+    return document_vector
+
+def cosine_simi(vec1, vec2):
+    dot_product = sum(vec1[key] * vec2.get(key, 0) for key in vec1)
+    magnitude1 = math.sqrt(sum(value ** 2 for value in vec1.values()))
+    magnitude2 = math.sqrt(sum(value ** 2 for value in vec2.values()))
+    return dot_product / (magnitude1 * magnitude2)
+
+def ngram_similarity(text1, text2, n):
+    token1= tokenize_text(text1, n)
+    token2= tokenize_text(text2, n)
+    vocab = create_vocabulary(token1, token2)
+    vec1 = document_representation(token1, vocab)
+    vec2 = document_representation(token2, vocab)
+    return cosine_simi(vec1, vec2)
 
 text1 = 'I have a dog and I love it'
 text2 = 'I have a cat and I hate it'
